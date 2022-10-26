afbcc0001cc5166c28321be15842f0e6d2019a12
Angel <Angel@LAPTOP-O744Q0QV>
2022-10-26 10:17:47 -0700

    Se creo el proyecto Catrina, el paquete de entidades y sus clases, y el paquete de enumeradores y sus clases

---
+++ src/mx/itson/catrina/entidades/Movimientos.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.catrina.entidades;
+import java.util.Date;
+import mx.itson.catrina.enumeradores.Tipo;
+
+
+
+/**
+ *
+ * @author Angel Fierros
+ */
+public class Movimientos {
+
+    private String descripcion;
+    private Date fecha;
+    private double cantidad;
+    private Tipo tipo;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+}
---
+++ src/mx/itson/catrina/enumeradores/Tipo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.catrina.enumeradores;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author Angel Fierros
+ */
+public enum Tipo {
+    @SerializedName("1")
+    Déposito,
+    
+    @SerializedName("2")
+    Retiro
+}
